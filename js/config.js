84d2fe9c1797a5127be44936b0250c706fb5aae8
takusho-kaihatsu <admin@takusho.co.jp>
2026-07-02 17:19:27 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,14 +2,12 @@ const SPREADSHEET_ID = ["10D09i82E", "_lwGeX51Mh", "kfDTIdxvsO", "EaKRJWl3ttI6NZ
 
 const DEPT_ORDER = [
   "財務経理部", "経営企画部", "人事総務部",
-  "街づくり事業部", "街づくり開発部", "建築部", "施工管理部",
-  "住宅営業部", "注文住宅部", "感動デザイン部", "リノベ事業部",
-  "be naked事業室",
+  "街づくり営業部", "街づくり開発部", "街づくり建築部", "街づくり施工部",
+  "街づくり販売部", "ストリア&RAM事業本部", "感動デザイン部", "千葉リノベ事業部",
   "福岡支店（リノベ事業部・開発事業部・管理部）",
-  "Good and", "Goodies", "ハローはちどり不動産", "エレキャスリゾート",
-  "経営メンバー（TK7)+監査役",
-  "拓匠分譲VOLTEKKERS", "匠に拓く委員会", "SANSUKE", "新組/鮮組",
-  "本能に、感動を。の会"
+  "Good and", "Goodies", "エレキャスリゾート", "拓匠ホールディングス",
+  "経営メンバー（TK6)+顧問",
+  "Associate Officer", "SANSUKE"
 ];
 
 function getSheetName(deptName) {
